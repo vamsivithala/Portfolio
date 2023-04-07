09576d050a85cf2e4e3c0a144baf6bbc85e931d4
vamsivithala <saivamsivithala.com>
2023-04-07 18:07:17 +0530

    all components updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { SkillsComponent } from './pages/skills/skills.component';
-import { ExperienceComponent } from './pages/experience/experience.component';
-import { ContactComponent } from './pages/contact/contact.component';
+
 import { HeaderComponent } from './sharepage/header/header.component';
 
 
@@ -21,8 +20,8 @@ import { HeaderComponent } from './sharepage/header/header.component';
     AboutComponent,
     PortfolioComponent,
     SkillsComponent,
-    ExperienceComponent,
-    ContactComponent,
+
+
     HeaderComponent,
 
   ],
